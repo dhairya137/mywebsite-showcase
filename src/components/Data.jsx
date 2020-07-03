6dcd3145268b83f844819b559259a7c22c0a19f1
Dhairya Patel <dhairya00798@gmail.com>
2020-07-03 13:03:14 +0530

    added 6 websites

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,25 +1,49 @@
 import pp1 from "../images/pp1.png";
 import pp2 from "../images/pp2.png";
 import pp3 from "../images/pp3.png";
+import pp4 from "../images/pp4.png";
+import pp5 from "../images/pp5.png";
+import pp6 from "../images/pp6.png";
 
 const Data = [
   {
     imgsrc: pp1,
     title: "Form Sign up with Validation",
-    siteLink: "https://www.google.com",
-    sourceCode: "https://www.google.com",
+    siteLink: "http://www.formvalidation.ga/",
+    sourceCode:
+      "https://github.com/dhairya137/JS-Project/tree/master/Project%201%20-%20Registration",
   },
   {
     imgsrc: pp2,
     title: "Movie Booking Site",
-    siteLink: "https://www.google.com",
-    sourceCode: "https://www.google.com",
+    siteLink: "https://nervous-mahavira-e2582c.netlify.app/",
+    sourceCode:
+      "https://github.com/dhairya137/JS-Project/tree/master/Project%20%203%20-%20Exchange%20Rate%20Calculator",
   },
   {
     imgsrc: pp3,
     title: "Exchange Rate Calculator",
-    siteLink: "https://www.google.com",
-    sourceCode: "https://www.google.com",
+    siteLink: "https://exchange-rate-calc-2020.netlify.app/",
+    sourceCode:
+      "https://github.com/dhairya137/JS-Project/tree/master/Project%202%20-%20Movie%20booking%20Website",
+  },
+  {
+    imgsrc: pp4,
+    title: "React Gallery App",
+    siteLink: "https://react-gallery-app-2020.netlify.app/",
+    sourceCode: "https://github.com/dhairya137/react-gallery-app",
+  },
+  {
+    imgsrc: pp5,
+    title: "New Year Countdown",
+    siteLink: "https://new-year-countdown2020.netlify.app/",
+    sourceCode: "/",
+  },
+  {
+    imgsrc: pp6,
+    title: "Lyrics Search",
+    siteLink: "https://lyrics-search-app-2020.netlify.app/",
+    sourceCode: "/",
   },
 ];
 
